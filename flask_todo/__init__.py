270744bca3bc28da5b04c83a3fd300746fcf681a
LazaroFranco <lazaro300x@icloud.com>
2019-03-23 18:25:48 -0400

    removed unneccesary files

---
+++ flask_todo/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+    
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/status')
+    def status_route():
+        code = request.args.get('c', 200)
+        response = make_response(f"status: {code}", code)
+
+        return response
+
+    return app
+
