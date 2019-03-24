01ee2d51cc2f8944377fa9a3764af8312ec5944a
LazaroFranco <lazaro300x@icloud.com>
2019-03-24 01:27:01 -0400

    testing

--- flask_todo/__init__.py
+++ flask_todo/__init__.py
@@ -1,8 +1,14 @@
-from flask import Flask, request, render_template
+import sys
+import os
 
+from flask import Flask, request, render_template, url_for, redirect
+
+from .task import Item
 
 
 def create_app(test_config=None):
+
+    from .task import Item
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_mapping(
@@ -13,10 +19,25 @@ def create_app(test_config=None):
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
-    
+
+    tasks = []
+
     @app.route('/')
+    def root():
+        return redirect(url_for('index'))
+    
+    @app.route('/todo', methods=["GET", "POST"])
     def index():
-        return render_template('index.html')
+        if request.method == 'POST':
+            new_item = Item(request.form['task'])
+            tasks.append(new_item)
+            return redirect(url_for('index'))
+        return render_template('index.html', tasks=tasks)
+
+    @app.route('/todo/create', methods=['POST'])
+    def create():
+        return render_template('create.html') 
+
 
     @app.route('/status')
     def status_route():
@@ -26,4 +47,56 @@ def create_app(test_config=None):
         return response
 
     return app
+   
+  ## @app.route('/calculate', methods=['GET', 'POST'])
+  # def calculate():
+  #     if request.method == 'GET':
+  #         return render_template('calculate.html', action="Add")
+  #     elif request.method == 'POST':
+  #         x = float(request.form['x'])
+  #         y = float(request.form['y'])
+  #         action = request.form['action']
+
+  #         if action == "Add":
+  #             result = x + y
+  #         elif action == "Subtract":
+  #             result = x - y
+  #         elif action == "Multiply":
+  #             result = x * y
+  #         elif action == "Divide":
+  #             result = x / y
+
+  #         return render_template('calculate.html', result=result, x=x, y=y)
+
+  # methods_route_allows = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
+
+  # @app.route('/method', methods=methods_route_allows)
+  # def method_route():
+  #     return render_template('method.html', allowed=methods_route_allows, method=request.method)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ flask_todo/task.py
@@ -0,0 +1,15 @@
+import datetime
+# A timestamp of when they were created DONE
+# A boolean marking the item as completed or not DONE
+# Text of the actual to-do item DONE
+
+class Item():
+
+
+    count = 1
+    def __init__(self, task):
+        self.task = task
+        self.timestamp = datetime.datetime.now()
+        #self.completed = False
+        self.id = Item.count
+        Item.count += 1
