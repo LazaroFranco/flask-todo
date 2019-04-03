b49d4cb1fb1248a4be5e9c5c81178d43c6ce01ab
LazaroFranco <lazaro300x@icloud.com>
2019-04-03 15:51:41 -0400

    add

---
+++ flask_todo/wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for gettingstarted project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+
+from . import create_app
+
+
+
+application = create_app()
